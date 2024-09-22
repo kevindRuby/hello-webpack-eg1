7f7133b0594cdc4a335c175b4f2ea453ab62f3be
kevindanTT <k.dandrade@live.com>
2024-09-22 14:39:01 +0000

    Update Your Webpack Configuration Modify your webpack.config.js to use the html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/main.js',
+    entry: './src/main.js', // Path to your entry file
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
+        clean: true, // Clean the output directory before each build
     },
     module: {
         rules: [
@@ -28,5 +30,14 @@ module.exports = {
             },
         ],
     },
-    mode: 'development',
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/main.html', // Path to your HTML template
+        }),
+    ],
+    mode: 'development', // Set to 'development' for dev environment
+    devServer: {
+        static: path.resolve(__dirname, 'dist'), // Serve files from the dist directory
+        open: true, // Automatically open the browser
+    },
 };
