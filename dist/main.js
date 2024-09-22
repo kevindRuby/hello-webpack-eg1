50ca92168d28e4162f388181ce8460195e9be5c7
kevindanTT <k.dandrade@live.com>
2024-09-22 15:02:49 +0000

    Minor fixes to image not displaying and blinking text not blinking

--- dist/main.js
+++ dist/main.js
@@ -7,26 +7,108 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
 /***/ "./src/main.js":
 /*!*********************!*\
   !*** ./src/main.js ***!
   \*********************/
-/***/ (() => {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("document.addEventListener(\"DOMContentLoaded\", function () {\n  var h1 = document.querySelector('h1');\n  setInterval(function () {\n    h1.style.visibility = h1.style.visibility === 'hidden' ? 'visible' : 'hidden';\n  }, 1000);\n});\n\n//# sourceURL=webpack://hello-webpack-eg1/./src/main.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _image_jpg__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./image.jpg */ \"./src/image.jpg\");\n // Adjust the path if necessary\n\nvar imgElement = document.createElement('img');\nimgElement.src = _image_jpg__WEBPACK_IMPORTED_MODULE_0__;\nimgElement.alt = 'Description of image';\ndocument.body.appendChild(imgElement);\nvar textElement = document.createElement('h1');\ntextElement.textContent = 'This text will blink!';\ndocument.body.appendChild(textElement);\nsetInterval(function () {\n  textElement.style.visibility = textElement.style.visibility === 'hidden' ? '' : 'hidden';\n}, 500);\n\n//# sourceURL=webpack://hello-webpack-eg1/./src/main.js?");
+
+/***/ }),
+
+/***/ "./src/image.jpg":
+/*!***********************!*\
+  !*** ./src/image.jpg ***!
+  \***********************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"a320c27ef55a9f0574e1.jpg\";\n\n//# sourceURL=webpack://hello-webpack-eg1/./src/image.jpg?");
 
 /***/ })
 
 /******/ 	});
 /************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/global */
+/******/ 	(() => {
+/******/ 		__webpack_require__.g = (function() {
+/******/ 			if (typeof globalThis === 'object') return globalThis;
+/******/ 			try {
+/******/ 				return this || new Function('return this')();
+/******/ 			} catch (e) {
+/******/ 				if (typeof window === 'object') return window;
+/******/ 			}
+/******/ 		})();
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/publicPath */
+/******/ 	(() => {
+/******/ 		var scriptUrl;
+/******/ 		if (__webpack_require__.g.importScripts) scriptUrl = __webpack_require__.g.location + "";
+/******/ 		var document = __webpack_require__.g.document;
+/******/ 		if (!scriptUrl && document) {
+/******/ 			if (document.currentScript && document.currentScript.tagName.toUpperCase() === 'SCRIPT')
+/******/ 				scriptUrl = document.currentScript.src;
+/******/ 			if (!scriptUrl) {
+/******/ 				var scripts = document.getElementsByTagName("script");
+/******/ 				if(scripts.length) {
+/******/ 					var i = scripts.length - 1;
+/******/ 					while (i > -1 && (!scriptUrl || !/^http(s?):/.test(scriptUrl))) scriptUrl = scripts[i--].src;
+/******/ 				}
+/******/ 			}
+/******/ 		}
+/******/ 		// When supporting browsers where an automatic publicPath is not supported you must specify an output.publicPath manually via configuration
+/******/ 		// or pass an empty string ("") and set the __webpack_public_path__ variable from your code to use your own logic.
+/******/ 		if (!scriptUrl) throw new Error("Automatic publicPath is not supported in this browser");
+/******/ 		scriptUrl = scriptUrl.replace(/#.*$/, "").replace(/\?.*$/, "").replace(/\/[^\/]+$/, "/");
+/******/ 		__webpack_require__.p = scriptUrl;
+/******/ 	})();
+/******/ 	
+/************************************************************************/
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/main.js"]();
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/main.js");
 /******/ 	
 /******/ })()
 ;
--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const h1 = document.querySelector('h1');
-    setInterval(() => {
-        h1.style.visibility = h1.style.visibility === 'hidden' ? 'visible' : 'hidden';
-    }, 1000);
-});
+import imageSrc from './image.jpg'; // Adjust the path if necessary
+
+const imgElement = document.createElement('img');
+imgElement.src = imageSrc;
+imgElement.alt = 'Description of image';
+document.body.appendChild(imgElement);
+
+
+const textElement = document.createElement('h1');
+textElement.textContent = 'This text will blink!';
+document.body.appendChild(textElement);
+
+setInterval(() => {
+    textElement.style.visibility = (textElement.style.visibility === 'hidden' ? '' : 'hidden');
+}, 500);
