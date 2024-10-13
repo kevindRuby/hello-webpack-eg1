0c72497c77222f2b49dc59892f09b241033341c3
kevindanTT <k.dandrade@live.com>
2024-10-13 13:04:19 +0000

    Edits to fix image duplication and image alt text issue

--- webpack.config.js
+++ webpack.config.js
@@ -33,6 +33,7 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: './src/main.html', // Path to your HTML template
+            inject: false, // Prevents automatic script injection            
         }),
     ],
     mode: 'development', // Set to 'development' for dev environment
